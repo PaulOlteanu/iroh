016fea7a29049d0e4dddbec8e99daf913102c806
Paul Olteanu <p.a.olteanu@gmail.com>
2024-11-18 22:30:10 -0500

    debian build

--- iroh-dns-server/src/main.rs
+++ iroh-dns-server/src/main.rs
@@ -7,13 +7,13 @@ use iroh_dns_server::{
 };
 use tracing::debug;
 
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+// #[cfg(not(target_env = "msvc"))]
+// #[global_allocator]
+// static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[allow(non_upper_case_globals, unsafe_code)]
-#[unsafe(export_name = "malloc_conf")]
-pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+// #[allow(non_upper_case_globals, unsafe_code)]
+// #[unsafe(export_name = "malloc_conf")]
+// pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
 
 #[derive(Parser, Debug)]
 struct Cli {
