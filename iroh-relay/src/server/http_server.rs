ee57230d288d1c7ec72fa9c99eb66a3c9d0a4fab
Paul Olteanu <p.a.olteanu@gmail.com>
2025-01-06 16:33:47 -0500

    even more idk

--- iroh-relay/src/server/http_server.rs
+++ iroh-relay/src/server/http_server.rs
@@ -247,7 +247,7 @@ impl ServerBuilder {
         socket.set_nodelay(true)?;
         socket.bind(addr)?;
         let listener = socket
-            .listen(8192)
+            .listen(32768)
             .with_context(|| format!("failed to bind server socket to {addr}"))?;
 
         // let listener = TcpListener::bind(&addr)
